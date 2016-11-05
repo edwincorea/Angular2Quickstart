28c9a9e145b914cc5fac4ffbb957b94e5b5da641
edwin <edwin@ASGARD>
2016-11-05 16:49:16 -0600

    Add project files.

---
+++ Angular2Quickstart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular2Quickstart.Startup))]
+namespace Angular2Quickstart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
